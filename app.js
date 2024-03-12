589200a250f51b9e3a9187c31f4f710a285607e4
rodrigo yamaya <rodrigoyamaya17@hotmail.com>
2024-03-12 20:42:04 -0300

    removendo titulo h1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'jogo do numero secreto');
+    
     exibirTextoNaTela('p', 'Escolha um numero entre 1 e 50');
 }
 
